834f2053944d76a5de3c7471466459d99b25bcc9
Mateusz Biernacki <303212@stud.umk.pl>
2023-05-16 15:30:08 +0200

    add unplugin-icons with mdi pack

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 
 <script>
     import {Button} from 'flowbite-svelte'
+    import Icon from 'virtual:icons/mdi/rocket-launch-outline'
 </script>
 
 <div class='p-4 text-lg'>
@@ -9,6 +10,7 @@
     <ul>
         <li class='my-2'><button class='rounded-xl bg-blue-400 text-white p-2 px-4'>Tailwind</button></li>
         <li class='my-2'><Button color='red'>Flowbite</Button></li>
+        <li>unplugin-icons with Material Design Icon pack <Icon class='inline-block w-10 h-10'/> </li>
     </ul>
     
 </div>
