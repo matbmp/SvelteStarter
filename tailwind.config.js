0d516e486e44a491227ef715312c66f7ab7bb966
matbmp <mbiernacki531@gmail.com>
2023-05-26 10:48:04 +0200

    remove flowbite, add skeleton and floating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
+import path from 'path';
+import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:  [
-    './src/**/*.{html,js,svelte,ts}',
-  "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
-],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		path.join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+    require('@tailwindcss/forms'),
+    ...skeleton(),
   ],
 
-  darkMode: 'class',
-}
-
+	darkMode: 'class'
+};
